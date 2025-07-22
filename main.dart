e9ba90c72d577d3f3bc21f7841828c0e3fba8778
keerthikV1802 <keerthikv1807@gmail.com>
2025-07-23 01:16:20 +0530

    main.dart

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/gradientcontainer.dart';
+
+void main() {
+  runApp(
+    const MaterialApp (
+      home: Scaffold(
+        body: GradientContainer()
+      ),
+   ),
+  );
+}
